a1616cd477f376004c7db34113c85578483eba25
Kazanami <dummy.github.1945@gmail.com>
2022-07-27 14:25:04 +0900

    Relocate Files

--- make/libexec/homebrew.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Updating APT Repo..."
-sudo apt update 
-echo "Installing build-essential procps curl file git"
-sudo apt install -y  build-essential procps curl file git
-
-echo "Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
