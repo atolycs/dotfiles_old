f930b6aed8b38870a7feb98ddbd04e56df8cead8
Kazanami <dummy.github.1945@gmail.com>
2022-07-27 13:50:52 +0900

    Update installer.sh

--- make/libexec/installer.sh
+++ make/libexec/installer.sh
@@ -8,35 +8,46 @@ do_help() {
     echo "usage: $(basename ${0}) [mode] [options]"
 }
 
-do_install() {
+do_dotinstall() {
+  echo "INSTALLTO: ${TO_INSTALL}"
+  echo "selected_shell: ${selected_shell}"
+}
 
+do_brewinstall() {
+  echo "Install Select HomeBrew"
 }
 
 main(){
     local TO_INSTALL="${HOME}/dotfiles"
     local MODE=""
     local INSTALL_BREW
+    local selected_shell
 
     while (( $# > 0))
     do
     case $1 in
         -d|--dotfiles=*)
-            if [[ "${MODE}" == "install" ]];then
-                if [[ "$1" =~ ^--dotfiles= ]];then
-                    TO_INSTALL=$(echo $1 | sed -e 's/^--dotfiles=//')
-                else
-                    TO_INSTALL="$2"
-                    shift;
-                fi
-            elif [[ "${MODE}" != "help" ]];then
-                echo "This option is only 'install' mode"
-            fi
+             if [[ "$1" =~ ^--dotfiles= ]];then
+                 TO_INSTALL=$(echo $1 | sed -e 's/^--dotfiles=//')
+             else
+                 TO_INSTALL="$2"
+                 shift;
+             fi
             ;;
         -h|--help)
             do_help
             exit 0;;
         -b|--brew)
-            INSTALL_BREW=1
+            INSTALL_BREW=1;;
+
+	-s|--shell=*)
+	    if [[ "$1" =~ ^--shell= ]];then
+		 selected_shell=$(echo $1 | sed -e 's/^--shell=//')
+	    else
+		 selected_shell="$2"
+		 shift
+            fi
+		 ;;
         -*)
             echo "unkown_option: $1"
             do_help
@@ -49,7 +60,11 @@ main(){
     done
 
     case $MODE in
-        install) :;;
+        install) 
+		[[ -n ${INSTALL_BREW} ]] && do_brewinstall
+	        do_dotinstall;;
+        update)
+		echo "update mode";;
         *) do_help;;
     esac
 }
