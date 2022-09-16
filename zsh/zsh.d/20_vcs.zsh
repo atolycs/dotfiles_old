6d30ad7b88e1882fd8a55caa9ec0e29f46333809
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-16 16:08:10 +0900

    untracked file prompt

--- zsh/zsh.d/20_vcs.zsh
+++ zsh/zsh.d/20_vcs.zsh
@@ -4,4 +4,12 @@ zstyle ':vcs_info:*' stagedstr "%F{yellow}!"
 zstyle ':vcs_info:*' unstagedstr "%F{red}+"
 zstyle ':vcs_info:*' formats '%F{green}(%s)-[%b]%c%u%f '
 zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
-__git_files() { _files }
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+     git status --branch --porcelain | grep -m 1 '^??' &>/dev/null
+  then
+    hook_com[unstaged]+='?'
+  fi
+}
+#__git_files() { _files }
