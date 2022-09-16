5ec3cf0cfa7cabb0584b0a85d2cbbc8641799e06
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-09-16 17:32:11 +0900

    untracked file prompt color change

--- zsh/zsh.d/20_vcs.zsh
+++ zsh/zsh.d/20_vcs.zsh
@@ -1,15 +1,15 @@
 setopt prompt_subst
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:*' unstagedstr "%F{red}+"
-zstyle ':vcs_info:*' formats '%F{green}(%s)-[%b]%c%u%f '
+zstyle ':vcs_info:*' stagedstr "%F{yellow%}!"
+zstyle ':vcs_info:*' unstagedstr "%F{red%}+"
+zstyle ':vcs_info:*' formats '%F{green%}(%s)-[%b]%c%u%f '
 zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 +vi-git-untracked() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
      git status --branch --porcelain | grep -m 1 '^??' &>/dev/null
   then
-    hook_com[unstaged]+='?'
+    hook_com[unstaged]+='%F{yellow%}?'
   fi
 }
 #__git_files() { _files }
