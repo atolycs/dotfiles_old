94a1da6f442c2bfe4f1ba4897b2e6c7f82be3386
Atolycs <dummy.github.1945+atolycs@gmail.com>
2022-11-24 16:00:01 +0900

    [git-hooks] Hooks changed

--- make/git/init-git.mk
+++ make/git/init-git.mk
@@ -1,9 +1,11 @@
-.PHONY: init-git
+.PHONY: init-git commit_msg
 
 GIT_HOOKS :=$(TOP_DIR)/git_hooks
 GIT_TEMPLATE_DIR := $(HOME)/git_template
+GIT_COMMIT_MESSAGE := $(TOP_DIR)/commit_template
 
 init-git: $(GIT_HOOKS)
+commit_msg: $(GIT_TEMPLATE_DIR)/commit_msg
 
 $(GIT_HOOKS): $(HOME)/git_template/hooks
 	ln -s $@ $<
@@ -11,4 +13,8 @@ $(GIT_HOOKS): $(HOME)/git_template/hooks
 
 $(GIT_TEMPLATE_DIR)/hooks: 
 	mkdir -p $(GIT_TEMPLATE_DIR)
+
+$(GIT_TEMPLATE_DIR)/commit_msg: $(GIT_COMMIT_MESSAGE)
+	ln -s $< $@
+	git config --global commit.template .git/commit_msg
 
